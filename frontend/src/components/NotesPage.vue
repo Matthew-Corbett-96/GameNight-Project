861f02edb81a4fdc74b61508578b403f8bcf4baa
Matthew Corbett <mcorbett16bf@gmail.com>
2023-05-05 01:44:56 -0400

    Practicing with VUE

--- frontend/src/components/NotesPage.vue
+++ frontend/src/components/NotesPage.vue
@@ -17,9 +17,22 @@ function removeNote (note: string): void {
 </script>
 
 <template>
-   <h1>Notes</h1>
+   <main>
+      <div class="container">
+         <header>
+            <h1>Notes</h1>
+            <button>+</button>
+         </header>
+         <div class="card-container">
+            <div class="card">
+               <p class="main-text"></p>
+               <p class="date"></p>
+            </div>
+         </div>
+      </div>
+   </main>
 
-   <div>
+   <!-- <div>
       <input type="text" v-model="tempNote" />
       <button @click="add()">Submit</button>
       <ul>
@@ -28,11 +41,56 @@ function removeNote (note: string): void {
             <button @click="removeNote(note)">Done</button>
          </div>
       </ul>
-   </div>
+   </div> -->
 </template>
 
 <style scoped>
-h3 {
+
+main {
+   height: 100vh;
+   widows: 100vw;
    margin: 40px 0 0;
+}
+
+.container {
+   max-width: 1000px;
+   margin: 0 auto;
+   padding: 10px;
+}
+
+header {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+}
+
+h1 {
+   font-size: 75px;
+   font-weight: bold;
+   color: #333;
+   margin-bottom: 20px;
+}
+
+button {
+   border: none;
+   padding: 10px;
+   width: 50px;
+   height: 50px;
+   cursor: pointer;
+   background-color: rgb(21, 20, 20);
+   border-radius: 100%;
+   color: white;
+   font-size: 20px;
+}
+
+.card {
+   width: 225px;
+   height: 225px;
+   background-color: rgb(237, 182, 44);
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   margin-right: 20px;
+   margin-left: 20px;
 }
 </style>
