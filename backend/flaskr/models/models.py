bff397aecafa681b1d7cd33f13c0e4d52099edfb
Matthew Corbett <mcorbett16bf@gmail.com>
2023-06-09 00:13:34 -0400

    Added CRUD logic for Games Table

--- backend/flaskr/models/models.py
+++ backend/flaskr/models/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 
 db = SQLAlchemy()
 
@@ -7,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     username = db.Column(db.String(64), index=True, unique=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
@@ -18,71 +20,123 @@ class User(db.Model):
 
     def __repr__(self) -> str:
         return "<User {}>".format(self.username)
+    
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "gender": self.gender,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "password": self.password
+        }
 
 
 # Game model
 class Game(db.Model):
     __tablename__ = "games"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String(64), unique=True)
     description = db.Column(db.String(256))
     game_type = db.Column(db.String(64))
     min_players = db.Column(db.Integer)
     max_players = db.Column(db.Integer)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<Game {}>".format(self.name)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "game_type": self.game_type,
+            "min_players": self.min_players,
+            "max_players": self.max_players
+        }
 
 
 # GameNight model
 class GameNight(db.Model):
     __tablename__ = "game_nights"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     date = db.Column(db.DateTime)
 
     def __repr__(self):
         return "<GameNight {}>".format(self.id)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date
+        }
 
 
 # GameNightGame model
 class GameNightGame(db.Model):
     __tablename__ = "game_night_games"
 
-    id = db.Column(db.Integer, primary_key=True)
-    game_night_id = db.Column(db.Integer, db.ForeignKey("game_nights.id"))
-    game_id = db.Column(db.Integer, db.ForeignKey("games.id"))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    game_night_id = db.Column(UUID, db.ForeignKey("game_nights.id"))
+    game_id = db.Column(UUID, db.ForeignKey("games.id"))
 
     def __repr__(self):
         return "<GameNightGame {}>".format(self.id)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "game_night_id": self.game_night_id,
+            "game_id": self.game_id
+        }
 
 # Attendance model
 class Attendance(db.Model):
     __tablename__ = "attendance"
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    game_night_id = db.Column(db.Integer, db.ForeignKey("game_nights.id"))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(UUID, db.ForeignKey("users.id"))
+    game_night_id = db.Column(UUID, db.ForeignKey("game_nights.id"))
     status = db.Column(
         db.String(10)
     )  # Possible statuses could be: Attending, Not Attending, Maybe
 
     def __repr__(self):
         return "<Attendance {}>".format(self.id)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "game_night_id": self.game_night_id,
+            "status": self.status
+        }
 
 # Announcement model
 class Announcement(db.Model):
     __tablename__ = "announcements"
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(UUID, db.ForeignKey("users.id"))
     start_date = db.Column(db.DateTime)
     end_date = db.Column(db.DateTime)
     content = db.Column(db.String(200))
 
     def __repr__(self):
         return "<Announcement {}>".format(self.id)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "content": self.content
+        }
     
 def init_app(app: Flask):
     db.init_app(app)
--- backend/flaskr/routes/routes.py
+++ backend/flaskr/routes/routes.py
@@ -1,30 +1,84 @@
 from flask import Flask, Response, request, jsonify, make_response
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from ..models.models import db, User, Game, GameNight, GameNightGame
+from uuid import UUID # used for generating unique ids
+
 
 def setup_routes(app: Flask, api: Api) -> None:
+    class GameRestClass(Resource):
+        def get(self, game_id: str = None) -> Response:
+            if game_id is None:
+                games = Game.query.all()
+                games_dict = [
+                    game.to_dict() for game in games   # convert each game to a dictionary
+                ]
+                return make_response(jsonify(games_dict), 200)
 
-   fakeDatabase = {
-      1: 'Buy groceries',
-      2: 'Learn Python',
-      3: 'Learn Flask',
-      4: 'Build an API'
-   }
+            game_id_uuid = UUID(game_id)
+            game: Game = Game.query.filter_by(id=game_id_uuid).first()
+            if game:
+                return make_response(jsonify(game.to_dict()), 200)
+            
+            return make_response(jsonify({"message": "Game not found."}), 404)
 
-   # User model
-   class TodoSimple(Resource):
-      def get(self, todo_id=None):
-         if todo_id is None:
-            return make_response(jsonify(fakeDatabase), 200)
-         return make_response(jsonify({todo_id: fakeDatabase[todo_id]}), 200)
+        def post(self):
+            data = request.json
+            game: Game = Game(
+                name=data["name"],
+                description=data["description"],
+                game_type=data["game_type"],
+                min_players=data["min_players"],
+                max_players=data["max_players"],
+            )
+            db.session.add(game)
+            db.session.commit()
+            return make_response(jsonify(game.to_dict()), 200)
 
-      def put(self, todo_id):
-         data = request.json
-         fakeDatabase[todo_id] = data['name']
-         return {todo_id: fakeDatabase[todo_id]}
+        def put(self, game_id):
+            game_id = UUID(game_id)
+            data = request.json
+            game: Game = Game.query.filter_by(id=game_id).first()
+            if game:
+                game.name = data["name"]
+                game.description = data["description"]
+                game.game_type = data["game_type"]
+                game.min_players = data["min_players"]
+                game.max_players = data["max_players"]
+                db.session.commit()
+                return make_response(jsonify(game.to_dict()), 200)
+            
+            return make_response(jsonify({"message": "Game not found."}), 404)
 
-   api.add_resource(TodoSimple, '/items/', '/items/<int:todo_id>')
+        def delete(self, game_id):
+            game_id = UUID(game_id)
+            game: Game = Game.query.filter_by(id=game_id).first()
+            if game:
+               db.session.delete(game)
+               db.session.commit()
+               return make_response(f"{game} was removed.", 200)
+            
+            return make_response(jsonify({"message": "Game not found."}), 404)
 
+    api.add_resource(GameRestClass, "/games/", "/games/<string:game_id>")
 
-   @app.route('/healthcheck', methods=['GET'])
-   def heathcheck() -> Response:
-      return make_response(jsonify({'message': 'OK'}), 200)
+    # User model
+    class UserRestClass(Resource):
+        def get(self, user_id=None):
+            if user_id is None:
+                users = User.query.all()
+                users_dict = [user.to_dict() for user in users]
+                return make_response(jsonify(users_dict), 200)
+
+            user_id: UUID = UUID(user_id)
+            user: User = User.query.filter_by(id=user_id).first()
+            if user:
+               return make_response(jsonify(user.to_dict()), 200)
+            
+            return make_response(jsonify({"message": "User not found."}), 404)
+
+    api.add_resource(UserRestClass, "/users/", "/users/<string:user_id>")
+
+    @app.route("/healthcheck", methods=["GET"])
+    def heathcheck() -> Response:
+        return make_response(jsonify({"message": "OK"}), 200)
