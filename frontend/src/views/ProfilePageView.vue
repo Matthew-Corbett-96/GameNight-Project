7ba6d6966b97cca5661ee8a4bfa0aa04f164f083
pkorrie <patiencekorrie@gmail.com>
2024-01-25 14:05:00 -0500

    update
    worked on profile page, added Icon

--- frontend/src/views/ProfilePageView.vue
+++ frontend/src/views/ProfilePageView.vue
@@ -6,6 +6,18 @@ import { computed } from 'vue';
 import { onMounted } from 'vue';
 import { type AuthProfile } from '../main';
 import {Button} from '../@/components/ui/button'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../@/components/ui/tabs'
+import { Input } from '../@/components/ui/input'
+import { Label } from '../@/components/ui/label'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../@/components/ui/card'
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -24,17 +36,85 @@ const logUser = () => console.log(current_user.value);
 
 <template>
   <div>
-    <Button> 
-      Hello
-    </Button>
-    <h1 class="bg-primary text-primary-foreground hover:bg-primary/30">{{current_user.username}}'s Profile Page</h1>
-    <p class="bg-gray-50 text-center hover:bg-gray-950">Email: {{current_user.email}}</p>
-    <p>Email Verified: {{ auth_profile.email_verified }}</p>
-    <p>Username: {{current_user.username}}</p>
-    <p>First Name: {{current_user.first_name}}</p>
-    <p>Last Name: {{current_user.last_name}}</p>
-    <p>Created At: {{current_user.created_on}}</p>
-    <p>Updated At: {{current_user.updated_on}}</p>
+    <Card>
+    <CardHeader>
+      <CardTitle>Welcome Matthew{{current_user.username}}!</CardTitle>
+    </CardHeader>
+    <CardContent>
+
+      <Tabs default-value="account">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger value="account">
+        Account
+      </TabsTrigger>
+      <TabsTrigger value="password">
+        Password
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent value="account">
+      <Card>
+        <CardHeader>
+          <CardDescription>
+            Make changes to your account here. Click save when you're done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent class="space-y-2">
+          <div class="space-y-1">
+            <Label for="name">Email</Label>
+            <Input id="name" default-value="" />
+          </div>
+          <div class="space-y-1">
+            <Label for="name">First Name</Label>
+            <Input id="name" default-value="" />
+          </div>
+          <div class="space-y-1">
+            <Label for="username">Last Name</Label>
+            <Input id="username" default-value="" />
+          </div>
+          <div class="space-y-1">
+            <Label for="name">Email Verified</Label>
+            <Input id="name" default-value="" />
+          </div>
+          <div class="space-y-1">
+            <Label for="name">Created At</Label>
+            <Input id="name" default-value="" />
+          </div>
+          <div class="space-y-1">
+            <Label for="name">Updated At</Label>
+            <Input id="name" default-value={{current_user.updated_on}} />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Save changes</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+    <TabsContent value="password">
+      <Card>
+        <CardHeader>
+          <CardDescription>
+            Change your password here. After saving, you'll be logged out.
+          </CardDescription>
+        </CardHeader>
+        <CardContent class="space-y-2">
+          <div class="space-y-1">
+            <Label for="current">Current password</Label>
+            <Input id="current" type="password" />
+          </div>
+          <div class="space-y-1">
+            <Label for="new">New password</Label>
+            <Input id="new" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Save password</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+  </Tabs>
+      
+    </CardContent>
+  </Card>
   </div>
 </template>
 
