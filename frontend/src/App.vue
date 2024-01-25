b9ab6b02d68da9c0f6b4b5752a182c9ec0031587
pkorrie <patiencekorrie@gmail.com>
2024-01-25 14:05:00 -0500

    update
    worked on profile page, added Icon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useAuthStore } from './store/auth';
 import { RouterView } from 'vue-router';
+import { Avatar, AvatarFallback, AvatarImage } from '../src/@/components/ui/avatar'
 
 const links = [
   'About Us',
@@ -17,8 +18,10 @@ const authStore = useAuthStore();
   <v-app>
 
     <v-app-bar scroll-behavior="elevate">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Game Night</v-toolbar-title>
+      <Avatar>
+    <AvatarImage src="src\assets\logoo.jpg" alt="@radix-vue" />
+    <AvatarFallback>CN</AvatarFallback>
+  </Avatar>
       <v-spacer></v-spacer>
       <v-btn variant="text" v-if="authStore.isLoggedIn" to="/games">Games</v-btn>
       <v-btn variant="text" v-if="authStore.isLoggedIn" to="/profile">Profile</v-btn>
@@ -26,6 +29,7 @@ const authStore = useAuthStore();
       <v-btn variant="text" v-if="authStore.isLoggedIn" @click="authStore.Logout">Logout</v-btn>
       <v-btn variant="text" v-if="!authStore.isLoggedIn" @click="authStore.Login">Login</v-btn>
       <v-btn variant="text" v-if="!authStore.isLoggedIn" @click="authStore.Register">Register</v-btn>
+      
 
     </v-app-bar>
 
