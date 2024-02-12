417c484a57394f876a904a329f0b6c5f695c4b10
pkorrie <patiencekorrie@gmail.com>
2024-02-12 17:44:32 -0500

    Remove img for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,7 +20,7 @@ const authStore = useAuthStore();
       
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/dashboard" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="src\assets\logoo.jpg" class="h-8" alt="Flowbite Logo" />
+            <!-- <img src="src\assets\logoo.jpg" class="h-8" alt="Flowbite Logo" /> -->
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GameNight</span>
           </a>
         </div>
