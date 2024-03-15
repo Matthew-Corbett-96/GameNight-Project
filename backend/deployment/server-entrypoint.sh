88843229f02733315a027fa7b6b70bc88c7e173a
Matthew Corbett <mcorbett16bf@gmail.com>
2024-03-15 01:56:18 -0400

    Remove database readiness check in server-entrypoint.sh

--- backend/deployment/server-entrypoint.sh
+++ backend/deployment/server-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Wait for the database to be ready
-echo "Waiting for the database to be ready..."
-while ! nc -z $DATABASE_URL 5432; do
-   sleep 0.1
-done
-echo "Database is ready"
-
 # Run the server
 echo "Running the server..."
 python -m gunicorn flaskr.run:app
